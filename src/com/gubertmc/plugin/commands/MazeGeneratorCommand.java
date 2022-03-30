985de85292b2620caa43d480797235e705ad786c
btror <ticer1999@gmail.com>
2022-03-30 17:18:11 -0400

    added 3d dfs

--- src/com/gubertmc/plugin/commands/MazeGeneratorCommand.java
+++ src/com/gubertmc/plugin/commands/MazeGeneratorCommand.java
@@ -91,6 +91,13 @@ public record MazeGeneratorCommand(MazeGeneratorPlugin plugin) implements Comman
                             Integer.parseInt(args[1]),
                             percentage
                     );
+                } else if (args[0].equalsIgnoreCase("dfs3d")) {
+                    maze = new DepthFirstSearchMaze3D(
+                            plugin,
+                            location.getBlock(),
+                            Integer.parseInt(args[1]),
+                            percentage
+                    );
                 } else {
                     player.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "/astar <Dimensions> <Size> <BlockedPercentage>");
                     player.sendMessage(ChatColor.YELLOW + "" + "Dimensions -> 2 or 3");
